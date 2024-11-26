fe2f1e1b99fe5ed7303eb697492e03c1a320ed32
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2024-11-26 01:41:00 -0300

    add eslint standard and extension recomendations

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,48 +3,48 @@ const router = express.Router()
 const crudProductos = require('../data/crud.productos')
 
 router.get('/', async (req, res) => {
-    const productos = await crudProductos.leerProductos()
-    res.render('home', {
-        layout: 'index',
-        productos: productos,
-        titulo: 'Home',
-    })
+  const productos = await crudProductos.leerProductos()
+  res.render('home', {
+    layout: 'index',
+    productos,
+    titulo: 'Home'
+  })
 })
 
 router.get('/menu', async (req, res) => {
-    res.render('menu', {
-        layout: 'index',
-        titulo: 'Menú',
-    })
+  res.render('menu', {
+    layout: 'index',
+    titulo: 'Menú'
+  })
 })
 
-router.get('/promociones', async(req, res) => {
-    const productosPromocion = await crudProductos.getPromociones()
-    res.render('promociones', {
-        layout: 'index',
-        titulo: 'Promociones',
-        promociones: productosPromocion,
-    })
+router.get('/promociones', async (req, res) => {
+  const productosPromocion = await crudProductos.getPromociones()
+  res.render('promociones', {
+    layout: 'index',
+    titulo: 'Promociones',
+    promociones: productosPromocion
+  })
 })
 
 router.get('/carrito', (req, res) => {
-    res.render('carrito', {
-        layout: 'index',
-        titulo: 'Carrito',
-    })
+  res.render('carrito', {
+    layout: 'index',
+    titulo: 'Carrito'
+  })
 })
 
 router.get('/contacto', (req, res) => {
-    res.render('contacto', {
-        layout: 'index',
-        titulo: 'Contacto',
-    })
+  res.render('contacto', {
+    layout: 'index',
+    titulo: 'Contacto'
+  })
 })
 router.post('/contacto', (req, res) => {
-    res.render('gracias', {
-        layout: 'index',
-        titulo: 'Contacto',
-    })
+  res.render('gracias', {
+    layout: 'index',
+    titulo: 'Contacto'
+  })
 })
 
 module.exports = router
--- data/crud.productos.js
+++ data/crud.productos.js
@@ -1,47 +1,44 @@
 const fs = require('fs-extra')
-const archivoProducto = __dirname + '/productos.json';
+// eslint-disable-next-line n/no-path-concat
+const archivoProducto = __dirname + '/productos.json'
 
 class CrudProducto {
-
-    async leerProductos() {
-        try {
-            const productos = await fs.readJson(archivoProducto);
-            console.log("leerProductos: " + productos.length)
-            return productos;
-        } catch (error) {
-            console.log(error)
-            return [];
-        }
+  async leerProductos () {
+    try {
+      const productos = await fs.readJson(archivoProducto)
+      console.log('leerProductos: ' + productos.length)
+      return productos
+    } catch (error) {
+      console.log(error)
+      return []
     }
+  }
 
-    async guardarProducto(producto) {
-        try {
-            const productos = await this.leerProductos();
-            productos.push(producto);
-            await fs.writeJson(archivoProducto, productos, { spaces: 2 });
-            return true
-        } catch (error) {
-            console.log(error)
-            return false
-        }
-
+  async guardarProducto (producto) {
+    try {
+      const productos = await this.leerProductos()
+      productos.push(producto)
+      await fs.writeJson(archivoProducto, productos, { spaces: 2 })
+      return true
+    } catch (error) {
+      console.log(error)
+      return false
     }
+  }
 
-    async getPromociones() {
-        const productos = await this.leerProductos()
-        if (!productos) return []
+  async getPromociones () {
+    const productos = await this.leerProductos()
+    if (!productos) return []
 
-        const promos = productos.filter(p => p.promocion)
-        console.log("get promos: " + promos.length)
-        if(promos.length > 1) return promos
-        return [promos]
-    }
+    const promos = productos.filter((p) => p.promocion)
+    console.log('get promos: ' + promos.length)
+    if (promos.length > 1) return promos
+    return [promos]
+  }
 
-    async eliminarProducto(producto) {
+  async eliminarProducto (hola) {}
 
-    }
-
-    async editarProducto(producto) { }
+  async editarProducto (hola) {}
 }
 
 module.exports = new CrudProducto()
